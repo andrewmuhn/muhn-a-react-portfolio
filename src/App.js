45e06f1ea3430a8acae0738219807fbd1e5da38c
andrew <andrewmuhn@gmail.com>
2023-06-10 12:15:08 -0700

    feat: basic file structure, coditional nav render

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Project from './components/Project';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import PortfolioContainer from './components/PortfolioContainer';
 
-function App() {
-  return (
-    <div className="portfolio-app">
-      <Navbar />
-      <Header />
-      <Project />
-      <Footer />
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Header from './Header';
+// import Footer from './Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+const PortfolioContainer = () => {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div className="portfolio-app">
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default PortfolioContainer;
