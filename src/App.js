6babd24c498f04eb395403bdb3281a425e8ce8b9
andrew <andrewmuhn@gmail.com>
2023-06-09 20:13:19 -0700

    feat: initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Project from './components/Project';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  return <div className="bucket-app"></div>;
+  return (
+    <div className="portfolio-app">
+      <Navbar />
+      <Header />
+      <Project />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
