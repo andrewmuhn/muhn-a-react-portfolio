dab91a0d765bfaad29446c884682f4c563af111d
andrew <andrewmuhn@gmail.com>
2023-06-11 12:41:15 -0700

    feat: email  & form entryvalidation

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Contact() {
+  const [notification, setNotification] = useState('');
+
+  const handleNotification = (e) => {
+    if (e.target.value === '') {
+      setNotification(`${e.target.name} field is required`);
+    }
+  };
+
+  const handleEmailValidation = (e) => {
+    if (!e.target.value.match(/^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/i)) {
+      setNotification('must input a valid email address');
+    } else setNotification('');
+  };
+
   return (
     <section className="section-container">
       <div className="section-header">
@@ -8,13 +22,34 @@ export default function Contact() {
       </div>
       <div className="section-content">
         <form className="contact-form">
-          <label for="name">Name: </label>
-          <input type="text" name="name" id="name" className=""></input>
-          <label for="email">Email: </label>
-          <input type="text" name="email" id="email" className=""></input>
-          <label for="message">Message: </label>
-          <input type="text" name="message" id="message" className=""></input>
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className=""
+            onMouseLeave={handleNotification}
+          ></input>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            className=""
+            onMouseLeave={handleNotification}
+            onChange={handleEmailValidation}
+          ></input>
+          <label htmlFor="message">Message: </label>
+          <input
+            type="text"
+            name="message"
+            id="message"
+            className=""
+            onMouseLeave={handleNotification}
+          ></input>
+          <input type="submit" value="submit"></input>
         </form>
+        <p>{notification}</p>
       </div>
     </section>
   );
